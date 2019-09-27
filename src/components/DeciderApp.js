9ed56aff1968ef6e829ea6c06a481a267cf8f31d
Brian Gass <briangass3@gmail.com>
2019-09-27 13:05:27 -0400

    Add transform-class-properties plugin and update event handlers

--- src/components/DeciderApp.js
+++ src/components/DeciderApp.js
@@ -5,16 +5,32 @@ import Header from './Header';
 import Action from './Action';
 
 export default class DeciderApp extends React.Component {
-    constructor(props) {
-        super(props);
-        this.handleDeleteOptions = this.handleDeleteOptions.bind(this);
-        this.handleDeleteOption = this.handleDeleteOption.bind(this);
-        this.handleDecision = this.handleDecision.bind(this);
-        this.handleAddOption = this.handleAddOption.bind(this);
-        this.state = {
-            options: props.options
-        };
-    }
+    state = {
+        options: []
+    };
+    handleDeleteOptions = () => {
+        this.setState(()=> ({ options: [] }));
+    };
+    handleDeleteOption = (optionToRemove) => {
+        this.setState((prevState) => ({
+            options: prevState.options.filter((option) => optionToRemove !== option)
+        }));
+    };
+    handleDecision = () => {
+        const randomNum = Math.floor(Math.random() * this.state.options.length);
+        const option = this.state.options[randomNum];
+        alert(option);
+    };
+    handleAddOption = (option) => {
+        if(!option) {
+            return 'Please enter an option to add.'
+        } else if (this.state.options.indexOf(option) > -1) {
+            return 'This option already exists.'
+        }
+        this.setState((prevState) => ({
+            options: prevState.options.concat(option)
+        }));
+    };
     componentDidMount() {
         try {
             const json = localStorage.getItem('options');
@@ -35,29 +51,6 @@ export default class DeciderApp extends React.Component {
     }
     componentWillUnmount() {
         console.log('componentWillUnmount');
-    }
-    handleDeleteOptions() {
-        this.setState(()=> ({ options: [] }));
-    }
-    handleDeleteOption(optionToRemove) {
-        this.setState((prevState) => ({
-            options: prevState.options.filter((option) => optionToRemove !== option)
-        }));
-    }
-    handleDecision() {
-        const randomNum = Math.floor(Math.random() * this.state.options.length);
-        const option = this.state.options[randomNum];
-        alert(option);
-    }
-    handleAddOption(option) {
-        if(!option) {
-            return 'Please enter an option to add.'
-        } else if (this.state.options.indexOf(option) > -1) {
-            return 'This option already exists.'
-        }
-        this.setState((prevState) => ({
-            options: prevState.options.concat(option)
-        }));
     }
     render() {
         const subTitle = 'Helping you make the right choices.';
