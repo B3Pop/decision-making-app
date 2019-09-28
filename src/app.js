7096d1700e0a93d31c92e5a565ea78690f61ab26
Brian Gass <briangass3@gmail.com>
2019-09-28 17:19:41 -0400

    Set up styling components and normalize

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ console.log('app.js is running');
 import React from 'react';
 import ReactDOM from 'react-dom';
 import DeciderApp from './components/DeciderApp';
+import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 
 ReactDOM.render(<DeciderApp />, document.getElementById('app'));
