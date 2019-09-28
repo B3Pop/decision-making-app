53ed5fb82b8fe02e97c3527051d54b152e8ec18a
Brian Gass <briangass3@gmail.com>
2019-09-28 14:27:08 -0400

    Add css loader

--- src/app.js
+++ src/app.js
@@ -3,5 +3,6 @@ console.log('app.js is running');
 import React from 'react';
 import ReactDOM from 'react-dom';
 import DeciderApp from './components/DeciderApp';
+import './styles/styles.scss';
 
 ReactDOM.render(<DeciderApp />, document.getElementById('app'));
